7a25cc2d8e0bda00248bee6d7f7e6a6837ed1497
dimash <dinmukhamed.t@tinkerteam.ai>
2024-06-14 16:00:04 +0500

    Create Sequence Added creation logic, models

--- DigitalTrafficLight/Domain/Controllers/SequenceController.cs
+++ DigitalTrafficLight/Domain/Controllers/SequenceController.cs
@@ -1,17 +1,27 @@
 using Contracts.Sequence;
+using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Domain.Controller;
 
 [ApiController]
+[Route("[controller]")]
 public class SequenceController : ControllerBase
 {
-    [HttpPost("/sequence")]
+    [HttpPost()]
     public IActionResult CreateSequence(CreateSequenceRequest request)
     {
-        return Ok(request);
+        var sequence = new Sequence(
+            Guid.NewGuid()
+        );
+
+        var response = new SequenceResponse(
+            sequence.Id
+        );
+
+        return Ok(response);
     }
-    [HttpGet("/sequence/{id:guid}")]
+    [HttpGet("{id:guid}")]
     public IActionResult GetSequence(Guid id)
     {
        return Ok(id);
